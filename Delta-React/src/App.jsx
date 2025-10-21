65a82540e4f00d0ff55efad2e3fc5cc645e92b2c
Yash Raj <149456901+YashRaj84@users.noreply.github.com>
2025-10-21 11:23:55 +0000

    vite+react installed

---
+++ Delta-React/src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+
+function Button() {
+  return (
+    <button>Go</button>
+  )
+}
+function App() {
+
+  return (
+    <>
+    <h1>Hello World.<br/>
+    It's Delta 5.o Batch.<br/></h1>
+    <Button/>
+    </>
+  )
+}
+
+export default App
