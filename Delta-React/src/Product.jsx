763a2433730e6d9490cfca73e3fae4b993305241
Yash Raj <149456901+YashRaj84@users.noreply.github.com>
2025-11-01 07:25:35 +0000

    adding products

--- Delta-React/src/Product.jsx
+++ Delta-React/src/Product.jsx
@@ -2,7 +2,7 @@ import './Product.css'
 function Product() {
     return (
         <div className="Product">
-            <h3>Product Title</h3>
+            <h3>{title}</h3>
             <h5>Product Description</h5>
         </div>
     )
--- Delta-React/src/ProductTab.jsx
+++ Delta-React/src/ProductTab.jsx
@@ -3,9 +3,10 @@ import Product from "./Product.jsx"
 function ProductTab() {
     return (
         <>
-        <Product/>
-        <Product/>
-        <Product/>
+        <Product title="phone" price="30,000"/>
+        <Product title="laptop" price="60,000"/>
+        <Product title="pen" price="20"/>
+        
         </>
     )
 }
