af58ee12e52c84be56a39f380f04d0065d878838
Yash Raj <149456901+YashRaj84@users.noreply.github.com>
2025-12-27 19:25:11 +0000

    Price component created

---
+++ Delta-React/src/Price.jsx
@@ -0,0 +1,9 @@
+export default function Price() {
+    return (
+        <div>
+            <p>Old Price</p>
+            <p>New Price</p>
+
+        </div>
+    )
+}
--- Delta-React/src/Product.jsx
+++ Delta-React/src/Product.jsx
@@ -1,15 +1,14 @@
 import './Product.css';
-
+import Price from "./Price.jsx";
 function Product({title, price, features}) {
-    let isDiscounted = price > 30000;
-    let styles = {backgroundColor: isDiscounted ? 'grey' :'lightgrey', padding: '10px', margin: '10px'};
+    // let isDiscounted = price > 30000;
+    // let styles = {backgroundColor: isDiscounted ? 'grey' :'lightgrey', padding: '10px', margin: '10px'};
     return (
         <div className="Product" style={styles}>
-            <h3>Item: {title}</h3>
-            <h4>Price: {price}</h4>
-            {isDiscounted ? <p><b>Discount: 5%</b></p> : null}
-            {/* OR */}
-            {/* {price > 30000 && <p>Discount: 5%</p>} */}
+            <p>Title</p>
+            <p>Description</p>
+            <Price/>
+
         </div>
     )
 }
