59c3120df30916baceabcf9ff6d7bbafd62b8813
Yash Raj <149456901+YashRaj84@users.noreply.github.com>
2025-12-29 13:33:46 +0000

    intro to states in react

---
+++ react-state-class/src/Counter.jsx
@@ -0,0 +1,15 @@
+export default function Counter() {
+    let count = 0;
+
+    function increment() {
+        count += 1;
+        console.log(count);
+    }
+
+    return (
+        <div>
+            <h2>Counter: {count}</h2>
+            <button onClick={increment}>Increment</button>
+        </div>
+    )
+}
