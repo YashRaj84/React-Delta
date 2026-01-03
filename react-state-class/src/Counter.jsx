31bd56c7b04ee17afc9f4d0081875bca5deea4ac
Yash Raj <149456901+YashRaj84@users.noreply.github.com>
2026-01-03 20:44:57 +0000

    New project file created

--- react-state-class/src/Counter.jsx
+++ react-state-class/src/Counter.jsx
@@ -1,15 +1,20 @@
 import {useState} from 'react';
+
+function init() {
+    console.log("Init function called");
+    return Math.random();
+}
 export default function Counter() {
-    let [count, setCount] = useState(0); //initialize count to 0
-    
+    let [count, setCount] = useState(init()); //initialize count to 0
+    console.log("Counter rendered with count:", count);
     let increment = () => {
-        setCount(count+1);
+        // setCount(count+1);
         // setCount((currCount) => {
         //     return currCount + 1;
         // });
-        // setCount((currCount) => {
-        //     return currCount + 1;
-        // });      
+        setCount((currCount) => {
+            return currCount + 1;
+        });      
     }
 
     return (
